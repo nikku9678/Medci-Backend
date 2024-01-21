bf929944daafdc9b0955213172cce189ba647a90
Nikku <kumarnikku300@gmail.com>
2024-01-21 14:55:38 +0530

    add server folder

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require('cors');
+// const path = require("path");
+
+//dotenv conig
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest obejct
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+//port
+const port = process.env.PORT || 8080;
+//listen port
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
